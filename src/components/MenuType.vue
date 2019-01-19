b6d1295e684e8f458c34c27d9f83f1f166e301e1
liguobao <liguobao@nana-tech.com>
2019-01-20 01:07:17 +0800

    fix type query

--- src/components/MenuType.vue
+++ src/components/MenuType.vue
@@ -8,6 +8,12 @@
       <MenuItem name="喜剧">喜剧</MenuItem>
       <MenuItem name="科幻">科幻</MenuItem>
       <MenuItem name="恐怖">恐怖</MenuItem>
+      <MenuItem name="战争">战争</MenuItem>
+      <MenuItem name="犯罪">犯罪</MenuItem>
+      <MenuItem name="美剧">美剧</MenuItem>
+      <MenuItem name="连续剧">连续剧</MenuItem>
+      <MenuItem name="动漫">动漫</MenuItem>
+      <MenuItem name="综艺">综艺</MenuItem>
     </Menu>
   </div>
 </template>
