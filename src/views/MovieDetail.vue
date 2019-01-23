9be971b2fc71705d98729678c20c89e279165fe0
tumengting <2395650867@qq.com>
2019-01-23 19:58:14 +0800

    movieDetail

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -11,19 +11,17 @@
         <Layout style="width: 80%; margin: auto;">
             <Header style="background-color:  #fff;"></Header>
             <h1 style="padding-bottom: 10px; font-size: 26px;  background-color:  #fff;">{{movie.name}}</h1>
-            <Layout>
-                <Sider hide-trigger style="background-color:  #F5F7F9; margin: 20px;" width="25%">
+            <Content style="width: 75%;">
+                <div style="background-color:  #F5F7F9; margin: 20px; width: 25%; float: left;">
                     <img v-bind:src="movie.cover" style="width: 100%" />
-                </Sider>
-                <Content style="width: 75%;">
-                    <div style="margin: 20px 50px; font-size: 18px; text-align: left; ">
+                </div>
+                    <div style="margin-left: 20px; font-size: 18px; text-align: left; ">
                         <p>片名 : {{movie.name}}</p>
                         <!-- <p>上映时间 : {{movie.publishTime===null?'0000-00-00':movie.publishTime.toString().substring(0,10)}}</p> -->
                         <p>更新时间 : {{movie.updateTime===null?'0000-00-00':movie.updateTime.toString().substring(0,10)}}</p>
                         <p><a @click="selectMovieType">{{movie.type===null?'未知':movie.type}}</a></p>
                     </div>
                 </Content>
-            </Layout>
             <Divider orientation="left">电影详情</Divider>
             <p v-html="movie.intro">剧情介绍 : {{movie.intro}}</p>
             <Divider orientation="left">电影观看</Divider>
